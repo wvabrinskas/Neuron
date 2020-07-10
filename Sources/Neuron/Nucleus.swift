06ee3edbdac94474a7df71defc710aa47e45e549
William Vabrinskas <williamvabrinskas@Williams-MacBook-Pro.local>
2020-07-09 20:43:15 -0400

    fixed interal init

--- Sources/Neuron/Nucleus.swift
+++ Sources/Neuron/Nucleus.swift
@@ -13,4 +13,12 @@ public struct Nucleus {
   public var learningRate: CGFloat = 0.1
   public var bias: CGFloat = 0.1
   public var activationType: Activation = .reLu
+  
+  public init(learningRate: CGFloat,
+              bias: CGFloat,
+              activationType: Activation) {
+    self.learningRate = learningRate
+    self.bias = bias
+    self.activationType = activationType
+  }
 }
