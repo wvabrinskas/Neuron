eb63ca2d088567794c1cef826826dcba7a5605c7
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-28 15:33:28 -0500

    added loss function

--- Sources/Neuron/Brain.swift
+++ Sources/Neuron/Brain.swift
@@ -351,7 +351,7 @@ public class Brain {
       let correct = correctValues[i]
       let outputNeuron = outputs[i]
       let get = outputNeuron.activation()
-      outputs[i].delta = correct - get
+      outputs[i].delta = self.calcTotalLoss([get], correct: [correct])
       if debug {
         print("out: \(i), predicted: \(get), actual: \(correct)")
       }
