42e41a43c6cb719ff5ee7f123efb6decf7782d4d
William Vabrinskas <wvabrinskas@gmail.com>
2020-12-30 19:59:27 -0500

    added dergp

--- Sources/Neuron/Neuron.swift
+++ Sources/Neuron/Neuron.swift
@@ -127,8 +127,8 @@ public class Neuron {
       
       //let der = self.activationType.derivative(input: input.inputValue)
 
-      input.weight -= self.learningRate * input.inputValue * delta
-      biasWeight -= self.learningRate * delta
+      input.weight += self.learningRate * input.inputValue * delta
+      biasWeight += self.learningRate * delta
     }
   }
   
