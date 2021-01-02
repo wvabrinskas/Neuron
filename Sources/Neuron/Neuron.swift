001a5e2f0e1ac4eeabfd2c03b988046a6d8061ee
William Vabrinskas <wvabrinskas@gmail.com>
2021-01-02 17:27:02 -0500

    trying debugging

--- Sources/Neuron/Neuron.swift
+++ Sources/Neuron/Neuron.swift
@@ -93,7 +93,10 @@ public class Neuron {
     var sum: Float = 0
         
     for i in 0..<self.inputs.count {
-      print("INPUT :\(self.inputs[i].inputValue)")
+      if self.inputs[i].inputValue > 1 {
+        print("INPUT :\(self.inputs[i].inputValue) type:\(self.layer)")
+        fatalError("over one")
+      }
       sum += self.inputs[i].weight * self.inputs[i].inputValue
     }
     
