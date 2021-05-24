1bc08c779672076622115dce11977c44d8f7a87f
William Vabrinskas <wvabrinskas@gmail.com>
2021-05-24 14:16:49 -0400

    testing again

--- Sources/Neuron/Models/LossFunction.swift
+++ Sources/Neuron/Models/LossFunction.swift
@@ -34,7 +34,7 @@ public enum LossFunction {
         return max(1e-10, value)
       }
       //−(𝑦log(𝑝)+(1−𝑦)log(1−𝑝))
-      let result = -y * log(clipped(p)) - (1 - y) * log(clipped(1 - p))
+      let result = -1 * (y * log(clipped(p)) + (1 - y) * log(clipped(1 - p))
       return result
     }
 
@@ -52,7 +52,7 @@ public enum LossFunction {
       let y = correct
       let p = predicted
       
-      let result = -(y / p) + (1 - y) / (1 - p)
+      let result = -1 * ((y / p) - ((1 - y) / (1 - p)))
       return result
     }
   }
