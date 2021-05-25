1c58e3ff9b20f00b5751bda964a45fab10886cc2
William Vabrinskas <williamvabrinskas@Williams-MacBook-Pro.local>
2021-05-24 21:06:48 -0400

    fixed prop

--- Sources/Neuron/Brain.swift
+++ Sources/Neuron/Brain.swift
@@ -664,7 +664,7 @@ public class Brain: Logger, NetworkBuilder {
     //reverse so we can loop through from the beggining of the array starting at the output node
     let reverse: [Lobe] = self.lobes.reversed()
     
-    //subtracting 1 because we dont need to propagate through to the weights in the input layer
+    //subtracting 2 because we dont need to propagate through to the weights in the input layer
     //those will always be 0 since no computation happens at the input layer
     for i in 0..<reverse.count - 2 {
       let currentLayer = reverse[i].neurons
