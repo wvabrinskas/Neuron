4ee4544350474876e7c5ebb1aeb40c535f0f3183
William Vabrinskas <wvabrinskas@gmail.com>
2021-10-19 14:59:18 -0400

    removed test

--- Tests/NeuronTests/NeuronClassificationTests.swift
+++ Tests/NeuronTests/NeuronClassificationTests.swift
@@ -111,42 +111,42 @@ final class NeuronClassificationTests:  XCTestCase, BaseTestConfig, ModelBuilder
     }
   }
   
-  func testXportLoss() {
-    XCTAssertTrue(brain != nil, "Brain is empty")
-    
-    guard let brain = brain else {
-      return
-    }
-    
-    print("Training for loss export....")
-    
-    brain.train(data: self.trainingData, validation: self.validationData) { (complete) in
-      let lastFive = brain.loss[brain.loss.count - 5..<brain.loss.count]
-      var sum: Float = 0
-      lastFive.forEach { (last) in
-        sum += last
-      }
-      let average = sum / 5
-      XCTAssertTrue(average <= TestConstants.testingLossThreshold, "Network did not learn, average loss was \(average)")
-    }
-    
-    for i in 0..<ColorType.allCases.count {
-      let color = ColorType.allCases[i]
-      
-      let out = brain.feed(input: color.color())
-      print("Guess \(color.string): \(out)")
-      
-      XCTAssert(out.max() != nil, "No max value. Training failed")
-
-      if let max = out.max(), let first = out.firstIndex(of: max) {
-        XCTAssertTrue(first == i, "Color \(color.string) could not be identified")
-      }
-    }
-    
-    let url = brain.exportLoss()
-    print("📉 loss: \(url)")
-    XCTAssertTrue(url != nil, "Could not build exported model")
-  }
+//  func testXportLoss() {
+//    XCTAssertTrue(brain != nil, "Brain is empty")
+//    
+//    guard let brain = brain else {
+//      return
+//    }
+//    
+//    print("Training for loss export....")
+//    
+//    brain.train(data: self.trainingData, validation: self.validationData) { (complete) in
+//      let lastFive = brain.loss[brain.loss.count - 5..<brain.loss.count]
+//      var sum: Float = 0
+//      lastFive.forEach { (last) in
+//        sum += last
+//      }
+//      let average = sum / 5
+//      XCTAssertTrue(average <= TestConstants.testingLossThreshold, "Network did not learn, average loss was \(average)")
+//    }
+//    
+//    for i in 0..<ColorType.allCases.count {
+//      let color = ColorType.allCases[i]
+//      
+//      let out = brain.feed(input: color.color())
+//      print("Guess \(color.string): \(out)")
+//      
+//      XCTAssert(out.max() != nil, "No max value. Training failed")
+//
+//      if let max = out.max(), let first = out.firstIndex(of: max) {
+//        XCTAssertTrue(first == i, "Color \(color.string) could not be identified")
+//      }
+//    }
+//    
+//    let url = brain.exportLoss()
+//    print("📉 loss: \(url)")
+//    XCTAssertTrue(url != nil, "Could not build exported model")
+//  }
   
   //executes in alphabetical order
   func testXport() {
