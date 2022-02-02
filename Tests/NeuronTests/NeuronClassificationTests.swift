993b4a9479b4c6129a6686e7fd88e67ec3263400
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-02-02 10:54:03 -0500

    adjusted learning rate for tests

--- Tests/NeuronTests/NeuronClassificationTests.swift
+++ Tests/NeuronTests/NeuronClassificationTests.swift
@@ -12,7 +12,7 @@ final class NeuronClassificationTests:  XCTestCase, BaseTestConfig, ModelBuilder
   public lazy var brain: Brain? = {
     let bias: Float = 0.0001
     
-    let brain = Brain(learningRate: 0.1,
+    let brain = Brain(learningRate: 0.01,
                       epochs: 5000,
                       lossFunction: .crossEntropy,
                       lossThreshold: TestConstants.lossThreshold,
