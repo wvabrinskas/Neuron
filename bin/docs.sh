56b0fae78c4d6bd2a2fed5982108df4628d7c4a2
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-08-03 12:18:45 -0400

    added doc

---
+++ bin/docs.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+swift package --allow-writing-to-directory ./docs \
+    generate-documentation --target Neuron \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path Neuron \
+    --output-path ./docs
