089eed3095d3a2e55d5d3aa7aa8cd267a78e1cbe
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2022-08-03 12:19:56 -0400

    updated script

--- bin/docs.sh
+++ bin/docs.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-swift package --allow-writing-to-directory ./docs \
+swift package --allow-writing-to-directory ../docs \
     generate-documentation --target Neuron \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path Neuron \
-    --output-path ./docs
+    --output-path ../docs
