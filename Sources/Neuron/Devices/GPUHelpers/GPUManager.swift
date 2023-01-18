84bd7ec09b2300c4022395cc874e96a4e6879834
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2023-01-18 15:23:47 -0500

    updated

--- Sources/Neuron/Devices/GPUHelpers/GPUManager.swift
+++ Sources/Neuron/Devices/GPUHelpers/GPUManager.swift
@@ -12,13 +12,10 @@ public class GPUManager {
     case activation, derivation, conv2d, conv2d_array, conv2d_texture
   }
   
-  private var currentRunningPipelines: [MTLComputePipelineState] = []
+  private var currentRunningPipelines: [String: MTLComputePipelineState] = [:]
   private var device: MTLDevice? = MTLCreateSystemDefaultDevice()
-  
-  @Atomic
-  private var textures: [Int: MTLTexture] = [:]
-  
-  lazy var queue = self.device?.makeCommandQueue()
+
+  lazy var queue = self.device?.makeCommandQueue(maxCommandBufferCount: 64)
   lazy var cmds = queue?.makeCommandBuffer()
   
   private func getFunction(_ function: MetalFunction) -> MTLFunction? {
@@ -26,7 +23,7 @@ public class GPUManager {
   }
   
   private func pipelineIfExists(type: MetalFunction) -> MTLComputePipelineState? {
-    return self.currentRunningPipelines.filter({ $0.label == type.rawValue }).first
+    return self.currentRunningPipelines[type.rawValue]
   }
   
   private func addPipeline(for type: MetalFunction) -> MTLComputePipelineState? {
@@ -44,7 +41,7 @@ public class GPUManager {
                                                          options: [],
                                                          reflection: nil)
       
-      self.currentRunningPipelines.append(pipeline)
+      self.currentRunningPipelines[type.rawValue] = pipeline
       return pipeline
       
     } catch {
@@ -76,63 +73,65 @@ public class GPUManager {
                 inputSize: TensorSize,
                 activationType: Activation,
                 derivate: Bool = false) -> Tensor {
-    
-    guard let device = self.device, let queue = queue else {
-      return Tensor()
+    autoreleasepool {
+      
+      guard let device = self.device, let queue = queue else {
+        return Tensor()
+      }
+      
+      let inputTexture = input.asTexture(device: device,
+                                         commandQueue: queue,
+                                         size: inputSize,
+                                         usage: .shaderRead)
+      
+      let outputTexture = Tensor().asTexture(device: device,
+                                             commandQueue: queue,
+                                             size: inputSize,
+                                             usage: .shaderWrite)
+      
+      let function: MetalFunction = derivate ? .derivation : .activation
+      let pipeline: MTLComputePipelineState? = self.pipelineIfExists(type: function) ?? self.addPipeline(for: function)
+      
+      let newEncoder = cmds?.makeComputeCommandEncoder()
+      
+      guard let encoder = newEncoder, let pipelineStrong = pipeline else {
+        return Tensor()
+      }
+      
+      var activation = CUnsignedInt(activationType.index())
+      
+      encoder.setComputePipelineState(pipelineStrong)
+      encoder.setTexture(inputTexture, index: 0)
+      encoder.setTexture(outputTexture, index: 1)
+      encoder.setBytes(&activation, length: MemoryLayout<CUnsignedInt>.size, index: 2)
+      
+      switch activationType {
+      case .leakyRelu(let limit):
+        var limit = Float(limit)
+        encoder.setBytes(&limit, length: MemoryLayout<Float>.size, index: 3)
+      default:
+        break
+      }
+      
+      let w = pipelineStrong.threadExecutionWidth
+      let h = pipelineStrong.maxTotalThreadsPerThreadgroup / w
+      let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
+      
+      let threadgroupsPerGrid = MTLSize(width: max(1, inputSize.columns / 2),
+                                        height: max(1, inputSize.rows / 2),
+                                        depth: inputSize.depth)
+      
+      encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+      encoder.endEncoding()
+      
+      //execution step
+      cmds?.commit()
+      cmds?.waitUntilCompleted()
+      
+      let tensor = outputTexture?.get3d(commandQueue: queue, device: device)
+      let out = Tensor(tensor ?? [])
+      return out
     }
-    
-    let inputTexture = input.asTexture(device: device,
-                                       commandQueue: queue,
-                                       size: inputSize,
-                                       usage: .shaderRead)
-    
-    let outputTexture = Tensor().asTexture(device: device,
-                                           commandQueue: queue,
-                                           size: inputSize,
-                                           usage: .shaderWrite)
-    
-    let function: MetalFunction = derivate ? .derivation : .activation
-    let pipeline: MTLComputePipelineState? = self.pipelineIfExists(type: function) ?? self.addPipeline(for: function)
-    
-    let newEncoder = cmds?.makeComputeCommandEncoder()
-    
-    guard let encoder = newEncoder, let pipelineStrong = pipeline else {
-      return Tensor()
-    }
-    
-    var activation = CUnsignedInt(activationType.index())
-    
-    encoder.setComputePipelineState(pipelineStrong)
-    encoder.setTexture(inputTexture, index: 0)
-    encoder.setTexture(outputTexture, index: 1)
-    encoder.setBytes(&activation, length: MemoryLayout<CUnsignedInt>.size, index: 2)
-    
-    switch activationType {
-    case .leakyRelu(let limit):
-      var limit = Float(limit)
-      encoder.setBytes(&limit, length: MemoryLayout<Float>.size, index: 3)
-    default:
-      break
-    }
-    
-    let w = pipelineStrong.threadExecutionWidth
-    let h = pipelineStrong.maxTotalThreadsPerThreadgroup / w
-    let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
-    
-    let threadgroupsPerGrid = MTLSize(width: max(1, inputSize.columns / 2),
-                                      height: max(1, inputSize.rows / 2),
-                                      depth: inputSize.depth)
-    
-    encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
-    encoder.endEncoding()
-    
-    //execution step
-    cmds?.commit()
-    cmds?.waitUntilCompleted()
-    
-    let tensor = outputTexture?.get3d(commandQueue: queue, device: device)
-    let out = Tensor(tensor ?? [])
-    return out
   }
   
   /// returns a 3D tensor where each element is conv on the input with a filter with a depth of 1
@@ -143,82 +142,85 @@ public class GPUManager {
                      strides: (rows: Int, columns: Int),
                      inputSize: TensorSize) -> Tensor {
     
-    guard let device = self.device, let queue = queue else {
-      return Tensor()
-    }
-    
-    let function: MetalFunction = .conv2d_texture
-    let pipeline: MTLComputePipelineState? = self.pipelineIfExists(type: function) ?? self.addPipeline(for: function)
-    
-    let newEncoder = cmds?.makeComputeCommandEncoder()
-    
-    guard let encoder = newEncoder, let pipelineStrong = pipeline else {
-      return Tensor()
-    }
-    
-    let outputSize = conv2dOutputSize(padding: padding,
-                                      strides: strides,
-                                      filterCount: 1,
-                                      filterSize: filterSize,
-                                      inputSize: inputSize)
-    
-    let inputTexture = input.asTexture(device: device,
-                                       commandQueue: queue,
-                                       size: inputSize,
-                                       usage: .shaderRead)
-    
-    let outputTexture = Tensor().asTexture(device: device,
-                                           commandQueue: queue,
-                                           size: TensorSize(rows: outputSize.rows,
-                                                            columns: outputSize.columns,
-                                                            depth: inputSize.depth), //use inputSize.depth here because we can parallelize the convolutions but not the summation
-                                           usage: .shaderWrite)
-    
-    let filterTexture = filter.asTexture(device: device,
-                                         commandQueue: queue,
-                                         size: TensorSize(rows: filterSize.rows,
-                                                          columns: filterSize.columns,
-                                                          depth: inputSize.depth),
-                                         usage: .shaderRead)
-    
-    encoder.setComputePipelineState(pipelineStrong)
-    encoder.setTexture(inputTexture, index: 0)
-    encoder.setTexture(outputTexture, index: 1)
-    encoder.setTexture(filterTexture, index: 2)
-    
-    var inSize = SIMD3(CUnsignedInt(inputSize.rows), CUnsignedInt(inputSize.columns), CUnsignedInt(inputSize.depth))
-    var outSize = SIMD3(CUnsignedInt(outputSize.rows), CUnsignedInt(outputSize.columns), CUnsignedInt(outputSize.depth))
-    var kSize = SIMD2(CUnsignedInt(filterSize.rows), CUnsignedInt(filterSize.columns))
-    var strides = SIMD2(CUnsignedInt(strides.rows), CUnsignedInt(strides.columns))
-    var padding = padding == .same ? 1 : 0
-    
-    encoder.setBytes(&inSize, length: MemoryLayout<SIMD3<CUnsignedInt>>.size, index: 1)
-    encoder.setBytes(&outSize, length: MemoryLayout<SIMD3<CUnsignedInt>>.size, index: 2)
-    encoder.setBytes(&kSize, length: MemoryLayout<SIMD2<CUnsignedInt>>.size, index: 3)
-    encoder.setBytes(&strides, length: MemoryLayout<SIMD2<CUnsignedInt>>.size, index: 4)
-    encoder.setBytes(&padding, length: MemoryLayout<Int>.size, index: 5)
-    
-    let w = pipelineStrong.threadExecutionWidth
-    let h = pipelineStrong.maxTotalThreadsPerThreadgroup / w
-    let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
-    
-    let threadgroupsPerGrid = MTLSize(width: max(1, inputSize.columns / 2),
-                                      height: max(1, inputSize.rows / 2),
-                                      depth: inputSize.depth)
-    
-    encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
-    encoder.endEncoding()
-    
-    cmds?.commit()
-    cmds?.waitUntilCompleted()
+    autoreleasepool {
       
-    let out = outputTexture?.get3d(commandQueue: queue, device: device) ?? []
-    let reducableStartingArray = [[Float]].init(repeating: [Float].init(repeating: 0,
-                                                                        count: outputSize.columns),
-                                                count: outputSize.rows)
-    let outSummed = out.reduce(reducableStartingArray, +)
-    
-    return Tensor(outSummed)
+      guard let device = self.device, let queue = queue else {
+        return Tensor()
+      }
+      
+      let function: MetalFunction = .conv2d_texture
+      let pipeline: MTLComputePipelineState? = self.pipelineIfExists(type: function) ?? self.addPipeline(for: function)
+      
+      let newEncoder = cmds?.makeComputeCommandEncoder()
+      
+      guard let encoder = newEncoder, let pipelineStrong = pipeline else {
+        return Tensor()
+      }
+      
+      let outputSize = conv2dOutputSize(padding: padding,
+                                        strides: strides,
+                                        filterCount: 1,
+                                        filterSize: filterSize,
+                                        inputSize: inputSize)
+      
+      let inputTexture = input.asTexture(device: device,
+                                         commandQueue: queue,
+                                         size: inputSize,
+                                         usage: .shaderRead)
+      
+      let outputTexture = Tensor().asTexture(device: device,
+                                             commandQueue: queue,
+                                             size: TensorSize(rows: outputSize.rows,
+                                                              columns: outputSize.columns,
+                                                              depth: inputSize.depth), //use inputSize.depth here because we can parallelize the convolutions but not the summation
+                                             usage: .shaderWrite)
+      
+      let filterTexture = filter.asTexture(device: device,
+                                           commandQueue: queue,
+                                           size: TensorSize(rows: filterSize.rows,
+                                                            columns: filterSize.columns,
+                                                            depth: inputSize.depth),
+                                           usage: .shaderRead)
+      
+      encoder.setComputePipelineState(pipelineStrong)
+      encoder.setTexture(inputTexture, index: 0)
+      encoder.setTexture(outputTexture, index: 1)
+      encoder.setTexture(filterTexture, index: 2)
+      
+      var inSize = SIMD3(CUnsignedInt(inputSize.rows), CUnsignedInt(inputSize.columns), CUnsignedInt(inputSize.depth))
+      var outSize = SIMD3(CUnsignedInt(outputSize.rows), CUnsignedInt(outputSize.columns), CUnsignedInt(outputSize.depth))
+      var kSize = SIMD2(CUnsignedInt(filterSize.rows), CUnsignedInt(filterSize.columns))
+      var strides = SIMD2(CUnsignedInt(strides.rows), CUnsignedInt(strides.columns))
+      var padding = padding == .same ? 1 : 0
+      
+      encoder.setBytes(&inSize, length: MemoryLayout<SIMD3<CUnsignedInt>>.size, index: 1)
+      encoder.setBytes(&outSize, length: MemoryLayout<SIMD3<CUnsignedInt>>.size, index: 2)
+      encoder.setBytes(&kSize, length: MemoryLayout<SIMD2<CUnsignedInt>>.size, index: 3)
+      encoder.setBytes(&strides, length: MemoryLayout<SIMD2<CUnsignedInt>>.size, index: 4)
+      encoder.setBytes(&padding, length: MemoryLayout<Int>.size, index: 5)
+      
+      let w = pipelineStrong.threadExecutionWidth
+      let h = pipelineStrong.maxTotalThreadsPerThreadgroup / w
+      let threadsPerThreadgroup = MTLSizeMake(w, h, 1)
+      
+      let threadgroupsPerGrid = MTLSize(width: max(1, inputSize.columns / 2),
+                                        height: max(1, inputSize.rows / 2),
+                                        depth: inputSize.depth)
+      
+      encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+      encoder.endEncoding()
+      
+      cmds?.commit()
+      cmds?.waitUntilCompleted()
+      
+      let out = outputTexture?.get3d(commandQueue: queue, device: device) ?? []
+      let reducableStartingArray = [[Float]].init(repeating: [Float].init(repeating: 0,
+                                                                          count: outputSize.columns),
+                                                  count: outputSize.rows)
+      let outSummed = out.reduce(reducableStartingArray, +)
+      
+      return Tensor(outSummed)
+    }
   }
   
 }
