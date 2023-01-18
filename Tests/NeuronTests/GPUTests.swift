db15ae3a370414acf5fcad18961e17a7bc00722f
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2023-01-17 19:59:56 -0500

    got 2d textures working

--- Tests/NeuronTests/GPUTests.swift
+++ Tests/NeuronTests/GPUTests.swift
@@ -5,13 +5,26 @@ import NumSwift
 
 class GPUTests: XCTestCase {
   
-  func testConv2dMetal() {
-    let inputShape = (6,6,1)
-        
-    let inputTensor = Tensor([[[Float]]].init(repeating: [[Float]].init(repeating: [Float].init(repeating: 1,
-                                                                                                count: inputShape.0),
-                                                                        count: inputShape.1),
-                                              count: inputShape.2)  )
+  func testGPUActivationFunctionSzie_multiple() {
+    let inputSize = TensorSize(rows: 6, columns: 6, depth: 2)
+    
+    let inputTensor = Tensor.withRandom(size: inputSize, in: Float(0.9)...Float(1))
+    
+    let gpu = GPUManager()
+    gpu.activate(inputTensor,
+                 inputSize: inputSize,
+                 activationType: .reLu)
+  }
+  
+  
+  func testGPUActivationFunctionSize() {
+    let inputSize = TensorSize(rows: 28, columns: 28, depth: 16)
+    
+    let inputTensor = Tensor.withRandom(size: inputSize, in: Float(-1)...Float(1))
+    
+    let activation = LeakyReLu(limit: 0.1)
+    activation.inputSize = inputSize
+    activation.device = GPU()
     
     let out = activation.forward(tensor: inputTensor)
     
@@ -68,40 +81,13 @@ class GPUTests: XCTestCase {
         
     let inputs = [Tensor].init(repeating: inputTensor, count: batchSize * batchCount)
     
-    let n = Sequential {
-      [
-        Conv2d(filterCount: filterCount,
-               inputSize: inputSize,
-               padding: .same,
-               initializer: .heNormal),
-        BatchNormalize(),
-        LeakyReLu(limit: 0.1),
-        MaxPool(),
-        Conv2d(filterCount: 32,
-               padding: .same,
-               initializer: .heNormal),
-        BatchNormalize(),
-        LeakyReLu(limit: 0.2),
-        Dropout(0.5),
-        MaxPool(),
-        Flatten(),
-        Dense(64, initializer: .heNormal),
-        LeakyReLu(limit: 0.2),
-        Dense(10, initializer: .heNormal),
-        Softmax()
-      ]
-    }
+    let conv = Conv2d(filterCount: filterCount,
+                      inputSize: inputSize,
+                      padding: .same,
+                      initializer: .heNormal)
     
-    let optim = Adam(n,
-                     device: GPU(),
-                     learningRate: 0.001)
     
-    let batched = inputs.batched(into: batchSize)
     
-    for b in batched {
-      let labels = [Tensor].init(repeating: Tensor([0,0,0,0,1,0,0,0,0,0]), count: b.count)
-      let out = optim.fit(b, labels: labels, lossFunction: .crossEntropySoftmax)
-      //print(out)
-    }
+    
   }
 }
