a905f97224095b14f9e63212cd37d9f4849ce3dd
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2024-06-11 21:42:56 -0400

    fixed tests

--- Tests/NeuronTests/Utilities/Resource.swift
+++ Tests/NeuronTests/Utilities/Resource.swift
@@ -15,10 +15,12 @@ struct Resource {
   init(name: String, type: String, sourceFile: StaticString = #file) throws {
     self.name = name
     self.type = type
+    
+    let resourcePathComponent = "NeuronTests/Resources"
 
     let testCaseURL = URL(fileURLWithPath: "\(sourceFile)", isDirectory: false)
     let testsFolderURL = testCaseURL.deletingLastPathComponent()
-    let resourcesFolderURL = testsFolderURL.deletingLastPathComponent().appendingPathComponent("Resources", isDirectory: true)
+    let resourcesFolderURL = testsFolderURL.deletingLastPathComponent().appendingPathComponent(resourcePathComponent, isDirectory: true)
     self.url = resourcesFolderURL.appendingPathComponent("\(name).\(type)", isDirectory: false)
   }
 }
