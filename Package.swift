535a76292212a4ef0f32d1e97b43037487f626fb
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2024-07-22 13:13:06 -0400

    updated min versions

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Neuron",
-    platforms: [ .iOS(.v13),
-                 .tvOS(.v13),
-                 .watchOS(.v6),
+    platforms: [ .iOS(.v14),
+                 .tvOS(.v14),
+                 .watchOS(.v7),
                  .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
